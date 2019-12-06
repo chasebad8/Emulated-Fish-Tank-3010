fecfd5a850db378612ba7ea21c1c4c23c70d49d7
chasebad8 <51832821+chasebad8@users.noreply.github.com>
2019-12-05 21:04:37 -0500

    Update startDatabase.py

--- startDatabase.py
+++ startDatabase.py
@@ -1,3 +1,12 @@
+'''
+Chase Badalato
+101072570
+Group m2
+SYSC3010 Project
+
+This code starts the database loop.  It clears the tables, reinitializes the database and calls gatherInfo
+'''
+
 import mainDatabase as db
 import sqlite3
 import random
@@ -9,11 +18,13 @@ port = 1025
 server_address = ('', port)
 s.bind(server_address)
 
+#This calls the mainDatabase code
 db.clearTables()
 db.initializeDatabase()
 
 print('')
 while True:
+    #forever gather info
     db.gatherInfo(s, port, server_address)
     print('')
 
